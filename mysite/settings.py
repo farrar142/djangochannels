7158014eb820fcb70ae6c239af138b6bc2d7b267
farrar142 <gksdjf1690@gmail.com>
2022-02-16 00:00:23 +0900

    product stock 도입 전

--- mysite/settings.py
+++ mysite/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import platform
-
+from mysite.secret import DATABASES
 def ipchooser():
     if platform.system().strip()=="Windows":
         return '127.0.0.1'
@@ -92,16 +92,7 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.mysql',
-            'NAME': 'chat',
-            'USER': 'sandring',
-            'PASSWORD': 'eowjsrhkddurtlehdreowjsfh304qjsrlf28',
-            'HOST': ipchooser(),
-            'PORT': '3306',
-        },        
-    }
+DATABASES = DATABASES
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
 
